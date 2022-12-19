f6accbd9328db845c8abf9ae8135ded92a656494
HermanoCastro65 <hermanocastro18@gmail.com>
2022-12-19 16:23:25 -0300

    put error conditional

--- src/models/block/class/block.class.ts
+++ src/models/block/class/block.class.ts
@@ -2,7 +2,7 @@ import { ICreateBlock, TBlock } from "../../block.exports"
 
 export class Block implements ICreateBlock {
   create(block: TBlock): TBlock {
+    if (!block) throw new Error()
     return block
-    throw new Error()
   }
 }
