509b49a003f289eee96f8702e7df603914d63359
HermanoCastro65 <hermanocastro18@gmail.com>
2022-12-19 17:00:00 -0300

    implement genesis block create function

---
+++ src/core/block/genesis.app.ts
@@ -0,0 +1,13 @@
+import { Block, TCreateGenesisBlock } from "../../models/block.exports"
+
+const genesisBlcock = {
+  index: 0,
+  hash: "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7",
+  previousHash: null,
+  timestamp: 1465154705,
+  data: "my genesis block!!",
+}
+
+export const createGenesisBlock: TCreateGenesisBlock = () => {
+  return new Block().create(genesisBlcock)
+}
